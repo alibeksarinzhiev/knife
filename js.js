d846dbde79bfe91a3c6c8154ebfd722121e7885f
Vindiza Aziz <vindizaaziz@gmail.com>
2023-02-18 18:40:06 +0600

    create project

---
+++ js.js
@@ -0,0 +1,16 @@
+(function($) {
+    $(function() {
+
+        $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+            $(this)
+                .addClass('active').siblings().removeClass('active')
+                .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+
+    });
+})(jQuery);
+
+function myFunction() {
+    let element = document.getElementById("burger");
+    element.classList.toggle("active");
+}
